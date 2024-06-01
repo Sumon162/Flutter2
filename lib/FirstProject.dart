a38b0b160a7ba68adba52fb452c5eb8f7f624945
Sumon Islam <sumonislam8557@gmail.com>
2024-06-01 09:57:50 +0600

    Flutter2

--- lib/FirstProject.dart
+++ lib/FirstProject.dart
@@ -1,3 +1,6 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -17,11 +20,78 @@ class FlutteraApp extends StatelessWidget{
 
 class HomeActivity extends StatelessWidget{
   const HomeActivity({super.key});
+
+  MyScanBar(message,context){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("hello"),
+        titleSpacing: 2,
+        toolbarHeight: 60,
+        toolbarOpacity: 1,
+        elevation: 10,
+        backgroundColor: Colors.green,
+        //centerTitle: true,
+        //titleTextStyle: TextStyle(color: Colors.black),
+        //title: Text("hello"),
+        actions: [
+          IconButton(onPressed: (){MyScanBar("show message Icon",context);},icon: Icon(Icons.message),),
+          IconButton(onPressed: (){MyScanBar("show search Icon",context);},icon: Icon(Icons.search),),
+          IconButton(onPressed: (){MyScanBar("show setting Icon",context);},icon: Icon(Icons.settings),)
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: Icon(Icons.add),
+        onPressed: (){
+          MyScanBar("show add Icon",context);
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        elevation: 10,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.photo),label: "photo"),
+          BottomNavigationBarItem(icon: Icon(Icons.mail),label: "mail"),
+          BottomNavigationBarItem(icon: Icon(Icons.phone),label: "phone")
+        ],
+        onTap: (int Index){
+          if(Index==0){
+            MyScanBar("show photo",context);
+          }
+          if(Index==1){
+            MyScanBar("show mail",context);
+          }
+          if(Index==2){
+            MyScanBar("show phone",context);
+          }
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.all(0),
+          children: [UserAccountsDrawerHeader(
+            accountName: Text("Sumon Islam"),
+            accountEmail: Text("sumon@gmail.com"),
+            arrowColor: Colors.cyan,
+            currentAccountPicture: new CircleAvatar(
+              backgroundImage: new NetworkImage("url"),
+              backgroundColor: Colors.green,
+            ),
+          )
+          ],
+        ),
+      ),
+        backgroundColor: Colors.green,
+      body: Container(
+        padding: EdgeInsets.all(300),
+        margin: EdgeInsets.all(300),
+        child: Text("FirstApp"),
       ),
     );
   }
