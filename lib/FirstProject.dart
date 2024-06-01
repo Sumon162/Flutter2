1b7a6542a9f5a9e39eac755c3ad50ef6958f2029
Sumon Islam <sumonislam8557@gmail.com>
2024-06-01 10:35:11 +0600

    Flutter2

--- lib/FirstProject.dart
+++ lib/FirstProject.dart
@@ -1,27 +1,36 @@
-import 'dart:ffi';
-import 'dart:ui';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void mian(){
-  runApp(const FlutteraApp());
+void main(){
+  runApp(const MyApp());
 }
-class FlutteraApp extends StatelessWidget{
-  const FlutteraApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: HomeScreen(),);
+  }
+}
+
+/*void main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //debugShowCheckedModeBanner: false,
-      home: HomeActivity(),);
+      home: HomeScreen(),
+    );
   }
+}*/
 
-}
 
-class HomeActivity extends StatelessWidget{
-  const HomeActivity({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
-  MyScanBar(message,context){
+  MyScanBar(message, context) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(message)),
     );
@@ -40,55 +49,83 @@ class HomeActivity extends StatelessWidget{
         //titleTextStyle: TextStyle(color: Colors.black),
         //title: Text("hello"),
         actions: [
-          IconButton(onPressed: (){MyScanBar("show message Icon",context);},icon: Icon(Icons.message),),
-          IconButton(onPressed: (){MyScanBar("show search Icon",context);},icon: Icon(Icons.search),),
-          IconButton(onPressed: (){MyScanBar("show setting Icon",context);},icon: Icon(Icons.settings),)
+          IconButton(
+            onPressed: () {
+              MyScanBar("show message Icon", context);
+            },
+            icon: Icon(Icons.message),
+          ),
+          IconButton(
+            onPressed: () {
+              MyScanBar("show search Icon", context);
+            },
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {
+              MyScanBar("show setting Icon", context);
+            },
+            icon: Icon(Icons.settings),
+          )
         ],
       ),
       floatingActionButton: FloatingActionButton(
         elevation: 10,
+        backgroundColor: Colors.greenAccent,
         child: Icon(Icons.add),
-        onPressed: (){
-          MyScanBar("show add Icon",context);
+        onPressed: () {
+          MyScanBar("show add Icon", context);
         },
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
         currentIndex: 0,
         elevation: 10,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.photo),label: "photo"),
-          BottomNavigationBarItem(icon: Icon(Icons.mail),label: "mail"),
-          BottomNavigationBarItem(icon: Icon(Icons.phone),label: "phone")
+          BottomNavigationBarItem(icon: Icon(Icons.photo), label: "photo"),
+          BottomNavigationBarItem(icon: Icon(Icons.mail), label: "mail"),
+          BottomNavigationBarItem(icon: Icon(Icons.phone), label: "phone")
         ],
-        onTap: (int Index){
-          if(Index==0){
-            MyScanBar("show photo",context);
+        onTap: (int Index) {
+          if (Index == 0) {
+            MyScanBar("show photo", context);
           }
-          if(Index==1){
-            MyScanBar("show mail",context);
+          if (Index == 1) {
+            MyScanBar("show mail", context);
           }
-          if(Index==2){
-            MyScanBar("show phone",context);
+          if (Index == 2) {
+            MyScanBar("show phone", context);
           }
         },
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.all(0),
-          children: [UserAccountsDrawerHeader(
-            accountName: Text("Sumon Islam"),
-            accountEmail: Text("sumon@gmail.com"),
-            arrowColor: Colors.cyan,
-            currentAccountPicture: new CircleAvatar(
-              backgroundImage: new NetworkImage("url"),
-              backgroundColor: Colors.green,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text("Sumon Islam",style: TextStyle(fontSize: 20),),
+              accountEmail: Text("sumon@gmail.com",style: TextStyle(fontSize: 20),),
+              arrowColor: Colors.cyan,
+              currentAccountPicture: new CircleAvatar(
+                backgroundImage: new NetworkImage("https://media.istockphoto.com/id/1496615469/photo/serene-latin-woman-enjoy-sunset-with-gratitude.jpg?s=2048x2048&w=is&k=20&c=f2BOEDlrq_Z90_MAqN0iFt7c6ilF2ci4bSSwaM-JpcU="),
+                backgroundColor: Colors.green,
+              ),
             ),
-          )
+            ListTile(
+              tileColor: Colors.cyan,
+             leading: Icon(Icons.add_alarm,color: Colors.white,size: 100,),
+             title: Text("alarm",style: TextStyle(fontSize: 30,color: Colors.white),),
+              onTap: (){
+                MyScanBar("Show alarm", context);
+              },
+            )
           ],
         ),
+
       ),
-        backgroundColor: Colors.green,
+      //backgroundColor: Colors.green,
       body: Container(
+        color: Colors.cyan,
         padding: EdgeInsets.all(300),
         margin: EdgeInsets.all(300),
         child: Text("FirstApp"),
