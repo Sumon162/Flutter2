e9971a8ad42d6f95e711835ffff659c278aff083
Sumon Islam <sumonislam8557@gmail.com>
2024-06-02 15:43:20 +0600

    Button

--- lib/Button.dart
+++ lib/Button.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,21 +20,42 @@ class Button extends StatelessWidget {
 class HomeSize extends StatelessWidget{
   const HomeSize({super.key});
 
+  MyScanBar(message,context){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message)),
+    );
+  }
+  
+
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle buttonStyle=ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        backgroundColor: Colors.deepOrangeAccent,
+        foregroundColor: Colors.white70,
+        textStyle: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10))
+        )
+
+    );
+    
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
         title: Text("hello",selectionColor: Colors.cyan,),
 
       ),
-      backgroundColor: Colors.black38,
-      body: Column(
+      backgroundColor: Colors.blueGrey,
+      body: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          TextButton(onPressed: (){}, child: Text("TextButton",style: TextStyle(fontSize: 40,color: Colors.white),)),
-          ElevatedButton(onPressed: (){}, child: Text("ElevatedButton",style: TextStyle(fontSize: 40,color: Colors.white),)),
-          OutlinedButton(onPressed: (){}, child: Text("OutlineButton",style: TextStyle(fontSize: 40,color: Colors.white),))
+          TextButton(onPressed: (){ MyScanBar("TextButton",context);}, child: Text("TextButton",style: TextStyle(fontSize: 20,color: Colors.white),)),
+          ElevatedButton(onPressed: (){ MyScanBar("ElevatedButton",context);}, child: Text("ElevatedButton"),style: buttonStyle,),
+          OutlinedButton(onPressed: (){ MyScanBar("OutlinedButton",context);}, child: Text("OutlineButton",style: TextStyle(fontSize: 20,color: Colors.white),))
         ],
       )
     );
