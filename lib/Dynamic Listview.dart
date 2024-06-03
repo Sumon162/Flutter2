39c1d33b6787a68850d57431e8f3ae50381e8341
Sumon Islam <sumonislam8557@gmail.com>
2024-06-03 12:11:19 +0600

    Dynamic Listview

---
+++ lib/Dynamic Listview.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const DynamicListView());
+}
+class DynamicListView extends StatelessWidget {
+  const DynamicListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListViewBuilder(),);
+  }
+
+  }
+
+class ListViewBuilder extends StatelessWidget{
+   ListViewBuilder({super.key});
+
+  var MyItems=[
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Sumon"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Rifat"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Sifat"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Sohan"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Maruf"},
+    {'img':"https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg",'title':"Riyad"}
+  ];
+
+  MyScanBar(context,msg){
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text(msg))
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: Text("Dynamic Listview"),
+
+      ),
+      body: /*GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 0,
+          childAspectRatio: 1.4,
+        ),
+          itemCount: MyItems.length,
+          itemBuilder: (context,index){
+            return GestureDetector(
+              onTap: (){MyScanBar(context, MyItems[index]['title']);},
+
+              child: Container(
+                //padding: EdgeInsets.all(20),
+                margin: EdgeInsets.all(5),
+                width: double.infinity,
+                height: 250,
+                child: Image.network(MyItems[index]['img']!, fit: BoxFit.fill,),
+              ),
+            );
+          }
+      ),*/
+
+
+
+      ListView.builder(
+        itemCount: MyItems.length,
+        itemBuilder: (context,index){
+          return GestureDetector(
+             /*onTap: (){MyScanBar(context, MyItems[index]['title']!);},
+            child: Card(
+              child: Image.network(MyItems[index]['title']!),
+            ),*/
+            child: Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(10),
+              width: double.infinity,
+              height: 250,
+              child: Image.network(MyItems[index]['img']!, fit: BoxFit.fill,),
+            ),
+          );
+        }
+      ),
+
+
+
+    );
+  }
+}
